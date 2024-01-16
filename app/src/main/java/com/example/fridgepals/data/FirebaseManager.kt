1a3bd2d6796313ac519241a437c1046ad0307ebd
YvonneGassner <y.gassner.yg@gmail.com>
2024-01-16 14:05:25 +0100

    added registerUser function

--- app/src/main/java/com/example/fridgepals/data/FirebaseManager.kt
+++ app/src/main/java/com/example/fridgepals/data/FirebaseManager.kt
@@ -1,9 +1,15 @@
 package com.example.fridgepals.data
 
+import com.example.fridgepals.data.model.User
 import com.google.firebase.database.FirebaseDatabase
 
 object FirebaseManager {
     val database: FirebaseDatabase by lazy {
         FirebaseDatabase.getInstance("\"https://ccl3-fridgepals-default-rtdb.europe-west1.firebasedatabase.app\"")
     }
+
+    fun registerUser(user: User) {
+        FirebaseManager.database.reference.child("users").push().setValue(user)
+    }
 }
+
