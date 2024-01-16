582e43ca36c1964d5abec9427b75f3e5e77f7a95
YvonneGassner <y.gassner.yg@gmail.com>
2024-01-16 10:19:32 +0100

    added FirebaseManager object

---
+++ app/src/main/java/com/example/fridgepals/data/FirebaseManager.kt
@@ -0,0 +1,9 @@
+package com.example.fridgepals.data
+
+import com.google.firebase.database.FirebaseDatabase
+
+object FirebaseManager {
+    val database: FirebaseDatabase by lazy {
+        FirebaseDatabase.getInstance("\"https://ccl3-fridgepals-default-rtdb.europe-west1.firebasedatabase.app\"")
+    }
+}
