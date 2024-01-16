0bfe342b8e1d7bc9e0a538d8ff54cfaa82483917
YvonneGassner <y.gassner.yg@gmail.com>
2024-01-16 12:11:58 +0100

    added data class fridgeItem and FoodCategory

--- app/src/main/java/com/example/fridgepals/data/model/user.kt
+++ app/src/main/java/com/example/fridgepals/data/model/user.kt
@@ -3,14 +3,27 @@ package com.example.fridgepals.data.model
 data class User(
     val name: String,
     val email: String,
+    val password: String,
     val address: Address,
     val virtualFridgeId: String,
-    val reservedItems: List<String>
+    val fridge: Map<String, FridgeItem>
 )
- {
 
-}
 data class Address(
     val city: String,
     val street: String
+)
+
+data class FridgeItem(
+    val name: String,
+    val quantity: String,
+    val categoryId: String,
+    val pickupDay: String,
+    val pickupTime: String,
+    val reserved: Boolean
+)
+
+data class FoodCategory(
+    val id: String,
+    val categoryName: String
 )
