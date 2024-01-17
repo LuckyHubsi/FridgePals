e6a2bae85560bef2400cc34a4114e9d390bd3757
YvonneGassner <y.gassner.yg@gmail.com>
2024-01-17 19:51:08 +0100

    removed password property from user class

--- app/src/main/java/com/example/fridgepals/data/model/user.kt
+++ app/src/main/java/com/example/fridgepals/data/model/user.kt
@@ -3,7 +3,6 @@ package com.example.fridgepals.data.model
 data class User(
     val name: String = "",
     val email: String = "",
-    val password: String = "",
     val address: Address = Address(),
     val fridge: Map<String, FridgeItem> = emptyMap()
 )
